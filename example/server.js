67d98ed1574539f9940921a3a76b6981ba295762
orangemug <info@orangemug.co.uk>
2015-07-23 20:43:16 +0100

    Fixed logging.

--- example/server.js
+++ example/server.js
@@ -1,14 +1,16 @@
 var express = require('express');
 
 module.exports = function start(port) {
+  var port, host;
   var app = express();
 
   app.get('/*', function (req, res) {
-    res.send("Hello world from, port:", port, "url:", req.url);
+    res.send("Hello world from<br>port: "+port+"<br>url: "+req.path);
   });
   var server = app.listen(port, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+    console.log(server.address())
+    host = server.address().address;
+    port = server.address().port;
     console.log('Example app listening at http://%s:%s', host, port);
   });
 }
